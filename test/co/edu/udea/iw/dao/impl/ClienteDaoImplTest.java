b30c3d969ea3bb24bb9249d7c9aef75b241819b0
tinez7g <tinez7g@users.noreply.github.com>
2015-10-01 23:32:45 -0500

    Organización de código test cliente.

--- test/co/edu/udea/iw/dao/impl/ClienteDaoImplTest.java
+++ test/co/edu/udea/iw/dao/impl/ClienteDaoImplTest.java
@@ -58,22 +58,29 @@ public class ClienteDaoImplTest {
         Usuario usuario = null;
         
         Cliente clienteConsulta = null;
+        
+        String login = "juan";
+        String cedula = "1040040896";
+        String nombres = "James";
+        String apellidos = "Garzon Otalvaro";
+        String email = "jamesgarzon92@gmail.com";
+        
         try {
             // Act
         	usuario = new Usuario();
         	daoUsuario = new UsuarioDaoImp();
-        	usuario = daoUsuario.obtenerUsuario("juan");
+        	usuario = daoUsuario.obtenerUsuario(login);
             dao = new ClienteDaoImp();
             cliente = new Cliente();
-            cliente.setCedula("1040040896");
-            cliente.setNombres("James");
-            cliente.setApellidos("Garzon Otalvaro");
-            cliente.setEmail("jamesgarzon92@gmail.com");
+            cliente.setCedula(cedula);
+            cliente.setNombres(nombres);
+            cliente.setApellidos(apellidos);
+            cliente.setEmail(email);
             cliente.setUsuarioCrea(usuario);
             cliente.setFechaCreacion(new Date());
             dao.guardar(cliente);
             // Assert
-            clienteConsulta = dao.obtener("1040040896");
+            clienteConsulta = dao.obtener(cedula);
             assertTrue(clienteConsulta != null);
         } catch (MyException e) {
             fail(e.getMessage());
@@ -89,18 +96,24 @@ public class ClienteDaoImplTest {
         Usuario usuario = null;
         
         Cliente clienteConsulta = null;
+        
+        String login = "juan";
+        String cedula = "1040040896";
+        String nombresNuevo = "James Danilo";
+        String apellidos = "Garzon Otalvaro";
+        String email = "jamesgarzon92@gmail.com";
         try {
             // Act
         	usuario = new Usuario();
         	daoUsuario = new UsuarioDaoImp();
-        	usuario = daoUsuario.obtenerUsuario("juan");
+        	usuario = daoUsuario.obtenerUsuario(login);
             dao = new ClienteDaoImp();
             cliente = new Cliente();
-            cliente = dao.obtener("1040040896");
+            cliente = dao.obtener(cedula);
 //            cliente.setCedula("300");
-            cliente.setNombres("James Danilo");
-            cliente.setApellidos("Garzon Otalvaro");
-            cliente.setEmail("jamesgarzon92@gmail.com");
+            cliente.setNombres(nombresNuevo);
+            cliente.setApellidos(apellidos);
+            cliente.setEmail(email);
             cliente.setUsuarioCrea(usuario);
             cliente.setFechaCreacion(new Date());
             cliente.setUsuarioModifica(usuario);
@@ -109,24 +122,24 @@ public class ClienteDaoImplTest {
             cliente.setFechaEliminacion(new Date());
             dao.actualizar(cliente);
             // Assert
-            clienteConsulta = dao.obtener("1040040896");
-            assertTrue(clienteConsulta.getUsuarioModifica().getLogin().equals("juan"));
+            clienteConsulta = dao.obtener(cedula);
+            assertTrue(clienteConsulta.getUsuarioModifica().getLogin().equals(login));
         } catch (MyException e) {
            throw new MyException(e);
         }
 
 	}
 
-//	@Test
+	@Test
 	public void testEliminar() throws MyException {
 		ClienteDaoImp dao= null;
         Cliente cliente = null;   
-        Cliente clienteConsulta = null;
+        String cedula = "300";
         try {
             // Act
             dao = new ClienteDaoImp();
             cliente = new Cliente();
-            cliente = dao.obtener("300");
+            cliente = dao.obtener(cedula);
             dao.eliminar(cliente);
         } catch (MyException e) {
             throw new MyException(e);
